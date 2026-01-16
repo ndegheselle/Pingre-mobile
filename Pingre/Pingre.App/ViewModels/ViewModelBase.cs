43b5005609b43e492033be23626ec282377a1409
Nicolas de gheselle <nicolas@degheselle.fr>
2026-01-16 18:24:25 +0100

    Add app project to git

---
+++ Pingre/Pingre.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Pingre.App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Pingre/Pingre.App/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Styling;
+
+namespace Pingre.App.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
